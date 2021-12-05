2ed2854db4f5ee1d23a923a66bd4234f6c901721
fukaya <>
2021-12-05 22:01:53 +0900

    mail

--- mail/store.php
+++ mail/store.php
@@ -3,7 +3,13 @@ ini_set('display_errors', "On");
 require_once "../db/mail.php"; 
 require_once "../db/accounts.php"; 
 
-var_dump($_POST);
-die();
+$account_id_list = $_POST['account_id'];
+
+foreach ($account_id_list as $k => $account_id) {
+    var_dump($account_id);
+    die();
+}
+
+
 
 ?>
